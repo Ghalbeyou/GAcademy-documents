8ade6c94075b2f58d152881afd5c187f746d3823
Arash <97286379+Ghalbeyou@users.noreply.github.com>
2023-03-25 14:41:33 +0330

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <img class="icon-3AqZ2e" src="https://cdn.discordapp.com/icons/1088017782941159426/55463237ac12441566e498dbfca84e2c.webp?size=128" alt=" " width="48" height="48" aria-hidden="true">,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
