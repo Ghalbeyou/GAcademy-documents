8f91fddce521aba75ad737b39c9a37393a1c9696
Arash <97286379+Ghalbeyou@users.noreply.github.com>
2023-03-25 14:45:11 +0330

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <img class="icon-3AqZ2e" src="https://cdn.discordapp.com/icons/1088017782941159426/55463237ac12441566e498dbfca84e2c.webp?size=128" alt=" " width="48" height="48" aria-hidden="true">,
+  logo: <img class="icon-3AqZ2e" src="https://cdn.discordapp.com/icons/1088017782941159426/55463237ac12441566e498dbfca84e2c.webp?size=128" alt=" " width="48" height="48" aria-hidden="true"></img>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://discord.gg/zHCzdEHwfV',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/zHCzdEHwfV',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Ghalbeyou/GAcademy-documents',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'GAcademy',
   },
 }
 
